10aa3c3ba2ddda1d15d9a635dc598b8195d5eeee
Lucero273 <82126168+Lucero273@users.noreply.github.com>
2021-04-26 23:24:01 -0500

    Eliminé algunas imágenes

--- trabajogrupal8.github.io - copia/Página Web - copia/app.js
+++ trabajogrupal8.github.io - copia/Página Web - copia/app.js
@@ -4,7 +4,7 @@ const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a'
 const header = document.querySelector('.header.container');
 
 hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
+hamburger.classList.toggle('active');
 	mobile_menu.classList.toggle('active');
 });
 
