cab9aa762b248e8f3fb788c3aa0a289bb8524bca
Lucero273 <82126168+Lucero273@users.noreply.github.com>
2021-06-12 19:14:23 -0500

    Puse los efectos de dark-mode a la página

--- Página Web - copia/rock2.js
+++ Página Web - copia/rock2.js
@@ -1,16 +1,39 @@
+
 //ANIMACIONES DEL MENU DE LA PÁGINA
-function cambiarClase(){
+function cambiarClase() {
     let menuPrincipal = document.getElementById('menu-principal');
-        menuPrincipal.classList.toggle('menu-principal-open');
+    menuPrincipal.classList.toggle('menu-principal-open');
     let menuOpen = document.getElementById('menu-toggle');
-        menuOpen.classList.toggle('menu-open')
+    menuOpen.classList.toggle('menu-open')
 }
 
-//Animaciones al body
-function cambiarFondo1(){
-    document.getElementById('cambiar-fondo1').style.backgroundColor="#EFF6E0";
-}
 
-function cambiarFondo2(){
-    document.getElementById('cambiar-fondo2').style.backgroundColor="#EDE8E9";
+
+// DARK MODE
+
+function activarDarkMode() {
+    const fondoPagina = document.body;
+    const titulo1 = document.querySelectorAll(".section-title")[0];
+    const titulo2 = document.querySelectorAll(".section-title")[1];
+    const titulo3 = document.querySelectorAll(".section-title")[2];
+    const titulo4 = document.querySelectorAll(".section-title")[3];
+    const parrafo1 = document.querySelector("p.que_es");
+    const parrafo2 = document.querySelectorAll("p.que_es")[1];
+    const subtitulo = document.querySelector(".subtitulo");
+    const subtitulo2 = document.querySelectorAll(".subtitulo")[1];
+    const subtitulo3 = document.querySelectorAll(".subtitulo")[2];
+
+    titulo1.classList.toggle('dark-mode');
+    titulo4.classList.toggle('dark-mode');
+    parrafo1.classList.toggle('dark-mode');
+    parrafo2.classList.toggle('dark-mode');
+    subtitulo.classList.toggle('dark-mode');
+    subtitulo2.classList.toggle('dark-mode');
+    subtitulo3.classList.toggle('dark-mode');
+    titulo2.classList.toggle('dark-mode');
+    titulo3.classList.toggle('dark-mode');
+    fondoPagina.classList.toggle('dark-mode');
 }
+const botonDarkMode = document.querySelector('.boton-dark-mode');
+botonDarkMode.addEventListener('click', activarDarkMode);
+
