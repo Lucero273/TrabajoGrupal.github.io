5d68858ab52db684df98a12222faff5f7e59ff75
anderbeat1 <82071116+anderbeat1@users.noreply.github.com>
2021-06-18 16:41:47 -0500

    implementadno darkmode

--- Página Web - copia/javacumbia.js
+++ Página Web - copia/javacumbia.js
@@ -59,3 +59,120 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+//DarkMode//
+function activarDarkMode() {
+    const fondoPagina = document.body;
+    const titulo1 = document.querySelectorAll(".section-title")[0];
+    const titulo2 = document.querySelectorAll(".section-title")[1];
+    const titulo3 = document.querySelectorAll(".section-title")[2];
+    const titulo4 = document.querySelectorAll(".section-title")[3];
+    const titulo5 = document.querySelectorAll(".section-title")[4];
+    const titulo6 = document.querySelectorAll(".section-title")[5];
+    const parrafo1 = document.querySelector("p.que_es");
+    const parrafo2 = document.querySelectorAll("p.que_es")[1];
+    const subtitulo = document.querySelector(".subtitulo");
+    const subtitulo2 = document.querySelectorAll(".subtitulo")[1];
+    const subtitulo3 = document.querySelectorAll(".subtitulo")[2];
+
+    titulo1.classList.toggle('dark-mode');
+    titulo4.classList.toggle('dark-mode');
+    titulo5.classList.toggle('dark-mode');
+    titulo6.classList.toggle('dark-mode');
+    parrafo1.classList.toggle('dark-mode');
+    parrafo2.classList.toggle('dark-mode');
+    subtitulo.classList.toggle('dark-mode');
+    subtitulo2.classList.toggle('dark-mode');
+    subtitulo3.classList.toggle('dark-mode');
+    titulo2.classList.toggle('dark-mode');
+    titulo3.classList.toggle('dark-mode');
+    fondoPagina.classList.toggle('dark-mode');
+}
+const botonDarkMode = document.querySelector('.boton-dark-mode');
+botonDarkMode.addEventListener('click', activarDarkMode);
+
+function imprimirConsola() {
+    console.log("La pagina esta cargada");
+} 
+
+//seccion-comentario//
+const mensaje=document.getElementById('mensaje')
+const formulario=document.querySelector('.seccion-comentario')
+
+
+mensaje.addEventListener('input',leerMensaje)
+formulario.addEventListener('submit',validarFormulario)
+
+var seccionMensaje = { 
+    mensaje:''
+};
+
+function leerMensaje(evento){
+    seccionMensaje.mensaje=evento.target.value;
+    console.log(seccionMensaje);
+}
+
+function mostrarMensajeError(mensaje) {
+    const bloqueError=document.createElement('p');
+    bloqueError.textContent=mensaje;
+    bloqueError.classList.add('mensajeError');
+    formulario.appendChild(bloqueError);
+    setTimeout(function(){
+        bloqueError.remove();
+    }, 5000 );
+}
+
+function mostrarMensajeOk(mensaje) {
+    const bloqueok=document.createElement('p');
+    bloqueok.textContent=mensaje;
+    bloqueok.classList.add('mensajeOk');
+    formulario.appendChild(bloqueok);
+    setTimeout(function(){
+        bloqueok.remove();
+    }, 5000 );
+}
+
+function mostrarMensaje(mensaje,bandera){
+    const bloque=document.createElement('p');
+    bloque.textContent=mensaje;
+    if(bandera==='correcto'){
+        bloque.classList.add('mensajeOk');
+    } else {
+        bloque.classList.add('mensajeError');
+    }
+    formulario.appendChild(bloque)
+
+    setTimeout(function(){
+        bloque.remove();
+    }, 5000 );
+}
+
+function validarFormulario(evento) {
+    evento.preventDefault();
+    if(seccionMensaje.mensaje===''){
+        mostrarMensajeError("No has ingresado un comentarios que ofrecer");
+        return;
+    }
+    mostrarMensajeOk("Gracias por su comentario, lo tendremos en cuenta.")
+}
+
+//Inicion del dark mode//
+function activarDarkMode() {
+    
+    //Referencia al body de la página web, para poder cambiar su background color
+    const fondoWeb=document.body;
+    const mainContenedor=document.querySelector('main.contenedor'); //le estoy diciendo que haga referencia a un <main class="contenedor">...</main>
+    
+    fondoWeb.classList.toggle("dark-mode");
+    mainContenedor.classList.toggle("dark-mode");
+    mainContenedor.classList.toggle('sombra-dark-mode');
+    formulario.classList.toggle('formulario-dark-mode');
+}
+
+const botonDarkMode=document.querySelector('.boton-dark-mode');
+botonDarkMode.addEventListener('click',activarDarkMode);
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches){
+    activarDarkMode();
+}
+
