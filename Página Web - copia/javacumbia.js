64adde5cd9ff7c741e46dd0d283a985b4ab9f973
anderbeat1 <82071116+anderbeat1@users.noreply.github.com>
2021-06-18 01:28:06 -0500

    remodelando pagina y aplicnado comentario.

--- Página Web - copia/javacumbia.js
+++ Página Web - copia/javacumbia.js
@@ -5,13 +5,13 @@ let sliderSectionLast = sliderSection[sliderSection.length -1];
 const btnLeft = document.querySelector("#btn-left");
 const btnRight = document.querySelector("#btn-right");
 
-slider.insertAdjacentElement("afterbegin", sliderSectionLast)
+slider.insertAdjacentElement("afterbegin", sliderSectionLast);
 
 function Next() {
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft = "-200%";
     slider.style.transition = "all 0.5s";
-    setTimeout(function(){
+    secTimeout(function() {
         slider.style.transition = "none";
         slider.insertAdjacentElement("beforeend", sliderSectionFirst);
         slider.style.marginLeft = "-100%";
@@ -20,23 +20,42 @@ function Next() {
 btnRight.addEventListener("click", function(){
     Next();
 });
-
+btnRight.addEventListener("click", function(){
+    Prev();
+});
 function Prev() {
-    let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+    let sliderSectionFirst = document.querySelectorAll(".slider__section");
     let sliderSectionLast = sliderSection[sliderSection.length -1];
-    slider.style.marginLeft = "0%";
+    slider.style.marginLeft = "0";
     slider.style.transition = "all 0.5s";
     setTimeout(function(){
         slider.style.transition = "none";
-        slider.insertAdjacentElement("afterbegin", sliderSectionLast);
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
         slider.style.marginLeft = "-100%";
-    }, 500);
+    }), 500;
 }
-btnLeft.addEventListener("click", function(){
-    Prev();
+
+//hamburger//
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
 });
-
-setInterval(function(){
-    Next();
-}, 5000);
-
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
