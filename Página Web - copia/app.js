60a49db982d975a36dc5b35d3d2df5c2a8923513
palo2122 <82066684+palo2122@users.noreply.github.com>
2021-06-17 22:04:29 -0500

    ya termine de agregar el javascript al formulario

--- Página Web - copia/app.js
+++ Página Web - copia/app.js
@@ -23,3 +23,70 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+
+//-------------------------inicio de java para el formulario-----------------------------//
+
+const nombre=document.getElementById('nombre')
+const correo=document.getElementById('correo')
+const formulario=document.querySelector('.formulario-contacto')
+
+var camposRegistro={
+	nombre:'',
+	correo:''
+};
+
+function leerNombre(evento){
+	camposRegistro.nombre=evento.target.value;
+	console.table(camposRegistro);
+}
+function leerCorreo(evento){
+	camposRegistro.correo=evento.target.value;
+	console.table(camposRegistro);
+}
+
+nombre.addEventListener('input',leerNombre)
+correo.addEventListener('input',leerCorreo)
+formulario.addEventListener('submit',validarFormulario)
+
+function mensajeError(mensaje){
+	const bloqueError=document.createElement('p');
+	bloqueError.textContent=mensaje;
+	bloqueError.classList.add('mensajeError')
+	formulario.appendChild(bloqueError);
+	setTimeout(function(){
+		bloqueError.remove();
+	}, 5000 );
+}
+
+function mensajeOk(mensaje){
+	const bloqueOk=document.createElement('p');
+	bloqueOk.textContent=mensaje;
+	bloqueOk.classList.add('mensajeOk')
+	formulario.appendChild(bloqueOk);
+	setTimeout(function(){
+		bloqueError.remove();
+	}, 5000 );
+}
+function mostrarMensaje(mensaje,bandera) {
+    const bloque=document.createElement('p');
+    bloque.textContent=mensaje;
+    if (bandera==='Correcto') {
+        bloque.classList.add('mensajeOk');
+    } else {
+        bloque.classList.add('mensajeError');
+    }
+    formulario.appendChild(bloque);   
+    setTimeout(function() {
+        bloque.remove();
+    }, 5000 ); 
+}
+
+function validarFormulario(evento){
+	evento.preventDefault();
+	if (camposRegistro.nombre==='' || camposRegistro.correo==='') { 
+	mensajeError("Todos los campos deben llenarse para que puedan ser enviados.");
+	return;
+	}	
+	mensajeOk("Informacion enviada correctamente");
+}
