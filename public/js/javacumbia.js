0ccb5575d466745613ee72abf336b33b9b78b3b4
anderbeat1 <82071116+anderbeat1@users.noreply.github.com>
2021-07-16 18:44:45 -0500

    cambio de darkmode

--- public/js/javacumbia.js
+++ public/js/javacumbia.js
@@ -142,3 +142,33 @@ function validarFormulario(evento) {
     }
     mostrarMensajeOk("Gracias por su comentario, lo tendremos en cuenta.")
 }
+
+function activarDarkMode() {
+    const fondoPagina = document.body;
+    const titulo1 = document.querySelectorAll(".section-title")[0];
+    const titulo2 = document.querySelectorAll(".section-title")[1];
+    const titulo3 = document.querySelectorAll(".section-title")[2];
+    const titulo4 = document.querySelectorAll(".section-title")[3];
+    const titulo5 = document.querySelectorAll(".section-title")[4];
+    const titulo6 = document.querySelectorAll(".section-title")[5];
+    const parrafo1 = document.querySelector("p.que_es");
+    const parrafo2 = document.querySelectorAll("p.que_es")[1];
+    const subtitulo = document.querySelector(".subtitulo");
+    const subtitulo2 = document.querySelectorAll(".subtitulo")[1];
+    const subtitulo3 = document.querySelectorAll(".subtitulo")[2];
+
+    titulo1.classList.toggle('dark-mode');
+    titulo4.classList.toggle('dark-mode');
+    titulo5.classList.toggle('dark-mode');
+    titulo6.classList.toggle('dark-mode');
+    parrafo1.classList.toggle('dark-mode');
+    parrafo2.classList.toggle('dark-mode');
+    subtitulo.classList.toggle('dark-mode');
+    subtitulo2.classList.toggle('dark-mode');
+    subtitulo3.classList.toggle('dark-mode');
+    titulo2.classList.toggle('dark-mode');
+    titulo3.classList.toggle('dark-mode');
+    fondoPagina.classList.toggle('dark-mode');
+}
+const botonDarkMode = document.querySelector('.boton-dark-mode');
+botonDarkMode.addEventListener('click', activarDarkMode);
